a876e4cdeb799b0b63e3998a90890b49cbc5f301
deathsw <deathswaltz@163.com>
2020-04-23 16:13:07 +0800

    optimize

--- src/android/cn/akapril/ble/BlePrintPlugin.java
+++ src/android/cn/akapril/ble/BlePrintPlugin.java
@@ -2,10 +2,12 @@ package cn.akapril.ble;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 import cpcl.PrinterHelper;
 
 /**
@@ -15,22 +17,35 @@ public class BlePrintPlugin extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
+        Log.d("params",String.valueOf(args));
+        if (action.equals("blePrint")) {
+            JSONObject message = args.getJSONObject(0);
             this.blePrint(message, callbackContext);
+            return true;
+        }else if(action.equals("bleConnect")){
+            String selectedBDAddress = args.getString(0);
+            this.bleConnect(selectedBDAddress, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void blePrint(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
+    private void blePrint(JSONObject params, CallbackContext callbackContext) {
+        if (params != null ) {
             cordova.getThreadPool().execute(new Runnable() {
                 public void run() {
                     try {
-                        PrinterHelper.printAreaSize("0", "200","200","500","1");
-                        PrinterHelper.Barcode("BARCODE", "128","1","1","100", "200","200",true,"7","0","5","123123");
-
+                        PrinterHelper.printAreaSize(params.getString("offset"),
+                                params.getString("horizontal"),params.getString("vertical"),
+                                params.getString("height"),params.getString("qty"));
+                        PrinterHelper.Align(params.getString("align"));
+                        PrinterHelper.Barcode(params.getString("command"), params.getString("type"),
+                                params.getString("width"),params.getString("ratio"),
+                                params.getString("vHeight"), params.getString("vX"),
+                                params.getString("vY"),params.getBoolean("undertext"),
+                                params.getString("number"),params.getString("size"),
+                                params.getString("tOffset"),params.getString("data"));
+                        PrinterHelper.Form();
                         PrinterHelper.Print();
                     } catch (Exception e) {
                         e.printStackTrace();
@@ -42,5 +57,19 @@ public class BlePrintPlugin extends CordovaPlugin {
         } else {
             callbackContext.error("Expected one non-empty string argument.");
         }
+    }
+    private void bleConnect(String selectedBDAddress, CallbackContext callbackContext) {
+        if (selectedBDAddress != null && selectedBDAddress.length() > 0) {
+            try {
+                PrinterHelper.PortOpenBT(selectedBDAddress);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            callbackContext.success(); // Thread-safe.
+        }else{
+            callbackContext.error("Expected one non-empty string argument.");
+
+        }
+
     }
 }
--- www/BlePrintPlugin.js
+++ www/BlePrintPlugin.js
@@ -3,3 +3,6 @@ var exec = require('cordova/exec');
 exports.blePrint = function (arg0, success, error) {
     exec(success, error, 'BlePrintPlugin', 'blePrint', [arg0]);
 };
+exports.bleConnect = function (arg0, success, error) {
+    exec(success, error, 'BlePrintPlugin', 'bleConnect', [arg0]);
+};
